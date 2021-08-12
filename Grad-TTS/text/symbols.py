71f870f127c1872e67c0c4e48a0ba4d49ec2ae30
NSPDL <ns.pdl@outlook.com>
2021-08-13 00:59:27 +0700

    - add symbols

--- Grad-TTS/text/symbols.py
+++ Grad-TTS/text/symbols.py
@@ -1,14 +1,39 @@
 """ from https://github.com/keithito/tacotron """
 
-from text import cmudict
+'''
+Defines the set of symbols used in text input to the model.
+The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
 
-_pad        = '_'
-_punctuation = '!\'(),.:;? '
+_pad = '_'
+_punctuation = ';:,.!?¡¿—…"«»“” '
 _special = '-'
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# Phonemes
+""" from https://github.com/keithito/tacotron """
 
-# Prepend "@" to ARPAbet symbols to ensure uniqueness:
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
+'''
+Defines the set of symbols used in text input to the model.
+'''
+_pad = '_'
+_punctuation = ';:,.!?¡¿—…"«»“”() '
+_special = '-'
+_letters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# Phonemes
+_vowels = 'iyɨʉɯuɪʏʊeøɘəɵɤoɛœɜɞʌɔæɐaɶɑɒᵻ'
+_non_pulmonic_consonants = 'ʘɓǀɗǃʄǂɠǁʛ'
+_pulmonic_consonants = 'pbtdʈɖcɟkɡqɢʔɴŋɲɳnɱmʙrʀⱱɾɽɸβfvθðszʃʒʂʐçʝxɣχʁħʕhɦɬɮʋɹɻjɰlɭʎʟ'
+_suprasegmentals = 'ˈˌːˑ'
+_other_symbols = 'ʍwɥʜʢʡɕʑɺɧ'
+_diacrilics = 'ɚ˞ɫ'
+_extra_phons = ['g', 'ɝ', '̃', '̍', '̥', '̩', '̯','̪',
+                '͡']  # some extra symbols that I found in from wiktionary ipa annotations
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = list(
+    _pad + _letters + _punctuation + _special + _vowels + _non_pulmonic_consonants
+    + _pulmonic_consonants + _suprasegmentals + _other_symbols + _diacrilics) + _extra_phons
+
+# Special symbol ids
+SPACE_ID = symbols.index(" ")
+
+symbols_set = set(symbols)
